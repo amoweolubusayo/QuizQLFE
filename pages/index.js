fab0436ecd16f05dd133c16bbf1d89c78c0e2b3d
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-08-06 21:36:25 +0100

    added cards to landing page

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,109 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/layout.tsx";
 
+const items = [
+  {
+    name: "Quiz One",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    name: "Quiz Two",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    name: "Quiz Three",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    name: "Quiz Four",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
+    imageUrl:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+];
 export default function Home() {
-	const items = [
-		{ id: 1, name: "test page" },
-		{ id: 2, name: "test page" },
-		{ id: 3, name: "test page" },
-		{ id: 4, name: "test page" },
-		{ id: 5, name: "test page" },
-		{ id: 6, name: "test page" },
-		// More items...
-	];
-	return (
-		<div>
-			<Layout title="Home">
-				<ul role="list" className="space-y-3">
-					{items.map((item) => (
-						<li
-							key={item.id}
-							className="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md"
-						>
-							<p className="text-center">{item?.name}</p>
-						</li>
-					))}
-				</ul>
-			</Layout>
-		</div>
-	);
+  return (
+    <div>
+      <Layout title="Home">
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+        >
+          {items.map((item) => (
+            <li
+              key={item.email}
+              className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+            >
+              <div className="flex-1 flex flex-col p-8">
+                <img
+                  className="w-32 h-32 flex-shrink-0 mx-auto rounded-full"
+                  src={item.imageUrl}
+                  alt=""
+                />
+                <h3 className="mt-6 text-gray-900 text-sm font-medium">
+                  {item.name}
+                </h3>
+                <dl className="mt-1 flex-grow flex flex-col justify-between">
+                  <dt className="sr-only">Title</dt>
+                  <dd className="text-gray-500 text-sm">{item.title}</dd>
+                  <dt className="sr-only">Role</dt>
+                  <dd className="mt-3">
+                    <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                      {item.role}
+                    </span>
+                  </dd>
+                </dl>
+              </div>
+              <div>
+                <div className="-mt-px flex divide-x divide-gray-200">
+                  <div className="w-0 flex-1 flex">
+                    <a
+                      href={`mailto:${item.email}`}
+                      className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                    >
+                      <div
+                        className="w-5 h-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-3">Email</span>
+                    </a>
+                  </div>
+                  <div className="-ml-px w-0 flex-1 flex">
+                    <a
+                      href={`tel:${item.telephone}`}
+                      className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+                    >
+                      <div
+                        className="w-5 h-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-3">Call</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    </div>
+  );
 }
