071729a0d9115fab22e47f5f4fe8ad8b8ca23889
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-08-06 22:12:48 +0100

    added rainbowkit and wagmi

--- components/header.tsx
+++ components/header.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import "@rainbow-me/rainbowkit/styles.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
+import React from "react";
 
-const Header = ({title}) => {
-    return(
-        <>
-            <header className="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
-                <div className="flex items-center justify-between">
-                <h2 className="font-semibold text-slate-900">{title}</h2>
-                <button className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-                    
-                    Connect to Wallet
-                </button>
-                </div>
-                
-            </header>
-        </>
-    )
+const Header = ({ title }) => {
+  return (
+    <>
+      <header className="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
+        <div className="flex items-center justify-between">
+          <h2 className="font-semibold text-slate-900">{title}</h2>
+          <ConnectButton />
+        </div>
+      </header>
+    </>
+  );
+};
 
-}
-
-export default Header
+export default Header;
